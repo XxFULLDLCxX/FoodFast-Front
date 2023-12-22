1aab264926b3562f03c1bbbe8cc71758ab1faa08
José Matheus <XxFULLDLCxX@outlook.com>
2023-12-22 02:38:27 -0300

    refactor: implement the categories from the back

---
+++ src/utils/defauts.ts
@@ -0,0 +1,11 @@
+import combo from '../assets/images/categories/combo.png';
+import drink from '../assets/images/categories/drink.png';
+import sideDish from '../assets/images/categories/side_dish.png';
+import dessert from '../assets/images/categories/dessert.png';
+
+export const defaultCategories = {
+  combo,
+  drink,
+  sideDish,
+  dessert,
+};
