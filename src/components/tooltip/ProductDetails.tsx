2ee455e3103345a0f6ae11f5f4b5026017fb5460
José Matheus <XxFULLDLCxX@outlook.com>
2023-12-22 01:59:45 -0300

    fix: updating for deploy on vercel

--- src/components/tooltip/ProductDetails.tsx
+++ src/components/tooltip/ProductDetails.tsx
@@ -1,13 +1,13 @@
 import { styled } from 'styled-components';
-import burgerDefault from '../../assets/images/burger.png';
+// import burgerDefault from '../../assets/images/burger.png';
 
 export default function ProductDetails() {
-  const defaultDetails = {
-    name: 'Smash da casa',
-    description: '2x hambúrger 200g, queijo, cheddar, tomate, alface, picles, cebola, molho da casa',
-    value: 3050,
-    image: burgerDefault,
-  };
+  // const defaultDetails = {
+  //   name: 'Smash da casa',
+  //   description: '2x hambúrger 200g, queijo, cheddar, tomate, alface, picles, cebola, molho da casa',
+  //   value: 3050,
+  //   image: burgerDefault,
+  // };
   return <Container></Container>;
 }
 
